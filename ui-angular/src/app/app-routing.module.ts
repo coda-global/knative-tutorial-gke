4d7504b46a0abbb93cff21477bf2986d09ae7971
Abraham K <abraham.k@coda.global>
2019-09-21 11:52:15 +0530

    Added thank you page

--- ui-angular/src/app/app-routing.module.ts
+++ ui-angular/src/app/app-routing.module.ts
@@ -4,20 +4,21 @@ import { LoginComponent } from './login/login.component';
 import { MessagingComponent } from './messaging/messaging.component';
 
 const routes: Routes = [
-  {
-    path: 'messaging',
-    component: MessagingComponent,
-    data: { title: 'GDG Demo - Messages' }
-  },
+  // {
+  //   path: 'messaging',
+  //   component: MessagingComponent,
+  //   data: { title: 'GDG Demo - Messages' }
+  // },
   {
     path: 'login',
     component: LoginComponent,
-    data: { title: 'GDG Demo - Login' }
+    data: { title: 'GDG Demo - Thanks' }
   },
   { path: '',
     redirectTo: '/login',
     pathMatch: 'full'
-  }
+  },
+  { path: '**', component: LoginComponent }
 ];
 
 @NgModule({
